5debbee3dec593b335eedec7f4a597e35b86a1b5
FaithO3321 <faithomondi1331@gmail.com>
2023-08-22 05:08:07 -0700

    Zero

--- 0-printf.c
+++ 0-printf.c
@@ -19,21 +19,19 @@ int _printf(const char *format, ...)
 			switch (*format)
 			{
 				case 'c':
-					chars_printed += print_char(va_arg(args, int));
+					chars_printed += printf("%c", va_arg(args, int));
 					break;
 				case 's':
-					chars_printed += print_string(va_arg(args, char*));
+					chars_printed += printf("%s", va_arg(args, char*));
 					break;
 				case '%':
-					chars_printed += write(1, format, 1);
-					break;
-				default:
+					chars_printed += printf("%%");
 					break;
 			}
 		}
 		else
 		{
-			chars_printed += write(1, format, 1);
+			chars_printed += printf("%c", *format);
 		}
 		format++;
 	}
@@ -41,30 +39,4 @@ int _printf(const char *format, ...)
 	va_end(args);
 
 	return (chars_printed);
-}
-
-/**
- * print_char - print characters
- * @c: characters to be printed
- * Return: characters printed
-*/
-int print_char(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
- * print_string - prints string
- * @s: string to be printed
- * Return: string characters
-*/
-int print_string(char *s)
-{
-	int chars = 0;
-
-	while (*s != '\0')
-	{
-		chars += write(1, s++, 1);
-	}
-	return (chars);
 }
